59cf27417e497f6b247039eae8d643bd14721bbc
juliordonez229 <juliordonez229@gmail.com>
2025-05-07 15:46:18 +0000

    idioma

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,16 @@ import 'package:flutter_application_2/theme_provider.dart';
 import 'package:flutter_application_2/language_provider.dart';
 import 'package:flutter_application_2/currency_provider.dart';
 import 'package:provider/provider.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
-        Provider<AuthService>(create: (_) => AuthService(databaseHelper: DatabaseHelper.instance)),
+        Provider<AuthService>(
+            create: (_) =>
+                AuthService(databaseHelper: DatabaseHelper.instance)),
         Provider<DatabaseHelper>(create: (_) => DatabaseHelper.instance),
         ChangeNotifierProvider(create: (_) => CurrencyProvider()),
         ChangeNotifierProvider(create: (_) => LanguageProvider()),
@@ -24,7 +27,7 @@ void main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -33,36 +36,42 @@ class MyApp extends StatelessWidget {
         return Consumer<LanguageProvider>(
           builder: (context, languageProvider, child) {
             return MaterialApp(
-              title: languageProvider.currentLocale.languageCode == 'en' ? 'Expense Tracker' : 'Seguimiento de Gastos',
+              title: languageProvider.currentLocale.languageCode == 'en'
+                  ? 'Expense Tracker'
+                  : 'Seguimiento de Gastos',
               locale: languageProvider.currentLocale,
-              localizationsDelegates: const [], // Add delegates if you need them for other purposes, but not for your custom translations
+              localizationsDelegates: const [],
               theme: themeProvider.currentTheme,
-              debugShowCheckedModeBanner: false, // Keep this line
-              initialRoute: '/', // Keep this line
+              debugShowCheckedModeBanner: false,
+              initialRoute: '/',
               routes: {
                 '/': (context) => FutureBuilder<bool>(
-                  future: Provider.of<AuthService>(context, listen: false).checkLoginStatus(),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return const Scaffold(
-                      body: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                    );
-                    } else {
-                      if (snapshot.hasData && snapshot.data!) {
-                        return const HomePage();
-                      } else {
-                        return const LoginScreen();
-                      }
-                    }
-                  },
-                ),
+                      future: Provider.of<AuthService>(context, listen: false)
+                          .checkLoginStatus(),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Scaffold(
+                            body: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          );
+                        } else {
+                          if (snapshot.hasData && snapshot.data!) {
+                            return const HomePage();
+                          } else {
+                            return const LoginScreen();
+                          }
+                        }
+                      },
+                    ),
                 '/login': (context) => const LoginScreen(),
                 '/home': (context) => const HomePage(),
               },
+            );
+          },
         );
-      });
+      },
     );
   }
 }
